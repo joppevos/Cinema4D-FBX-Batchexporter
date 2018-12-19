6f9b1b637ffaa080b48144977dd042631ee8d796
joppevos <joppe.vos@gmail.com>
2018-12-19 17:09:18 +0100

    added todo

--- _main.py
+++ _main.py
@@ -1,5 +1,6 @@
 import os
 import c4d
+import time
 """
 # LOOK FOR IN A DIRECTORY WITH MULTIPLE FOLDERS, FIND THE C4D FILES IN THOSE FOLDERS. PLACE THEM IN EXACT THE 
 SAME NAME TYPE OF FOLDERS. 
@@ -14,22 +15,21 @@ def main():
     """
     export all c4d files in given directory to FBX
     """
-    importpath = (r'\\192.168.3.6\current04\DLL_GAMING\Animatic\c4d\Sc01_Arena')
+    importpath = (r'\\192.168.3.6\current04\DLL_GAMING\Animatic\3dExchange\fromC4D\arena_collect')
     files = os.listdir(importpath)
-
     if files:
-        for file in files:
-            print('opening file: {}'.format(file))
-            path = os.path.join(importpath, file)
+        for afile in files:
+            print('opening file: {}'.format(afile))
+            path = os.path.join(importpath, afile)
             c4d.documents.LoadFile(path)
-            # export FBX
-            exportpath = os.path.join(r'\\192.168.3.6\current04\DLL_GAMING\Animatic\3dExchange\fromC4D',
-                                      '{}.fbx'.format(file))
+            exportpath = os.path.join(r'\\192.168.3.6\current04\DLL_GAMING\Animatic\3dExchange\fromC4D\arena_fbx',
+                                      '{}.fbx'.format(afile.split('.')[0]))
+            doc = c4d.documents.GetActiveDocument()
             if c4d.documents.SaveDocument(doc, exportpath, c4d.SAVEDOCUMENTFLAGS_DONTADDTORECENTLIST, 1026370):
                 print('file succesful exported')
-                c4d.documents.KillDocument(doc)
             else:
                 print('error occured when writing file')
+        c4d.documents.CloseAllDocuments()
     else:
         print('folder is emtpy')
 
