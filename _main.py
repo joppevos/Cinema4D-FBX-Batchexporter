2f39ca14274e725c0b31d6eb198d079ee20cc756
joppevos <joppe.vos@gmail.com>
2018-12-19 14:26:00 +0100

    base commit

---
+++ _main.py
@@ -0,0 +1,29 @@
+import os
+import c4d
+
+
+def main():
+    """
+    export all c4d files in given directory to FBX
+    """
+    importpath = (r'\\192.168.3.6\current04\DLL_GAMING\Animatic\c4d\Sc01_Arena')
+    files = os.listdir(importpath)
+
+    if files:
+        for file in files:
+            print('opening file: {}'.format(file))
+            path = os.path.join(importpath, file)
+            c4d.documents.LoadFile(path)
+            # export FBX
+            exportpath = os.path.join(r'\\192.168.3.6\current04\DLL_GAMING\Animatic\3dExchange\fromC4D',
+                                      '{}.fbx'.format(file))
+            if c4d.documents.SaveDocument(doc, exportpath, c4d.SAVEDOCUMENTFLAGS_DONTADDTORECENTLIST, 1026370):
+                print('file succesful exported')
+                c4d.documents.KillDocument(doc)
+            else:
+                print('error occured when writing file')
+    else:
+        print('folder is emtpy')
+
+if __name__ == '__main__':
+    main()
